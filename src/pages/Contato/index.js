f7893376a4d90e0875553c23ead3f00780749554
Thiago <novaesthago@gmail.com>
2020-03-20 20:07:01 -0300

    Ajustes

--- src/pages/Contato/index.js
+++ src/pages/Contato/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import './style.css';
 
 function Contato() {
     return (
         <div className='page'>
+
+            <FontAwesomeIcon icon={faCoffee} />
+            
             <div className='content-item'>
 
                 <div className='titulo-page'>
